a71fef751e77f88577643440bac2ac704e1f4ff3
Samia-almtlek <samia.almoutlak@gmail.com>
2025-11-17 19:37:31 +0100

    Implement CustomUserDetailsService for SpringSecurity authentication

---
+++ src/main/java/com/ehb/rental/rentalplatform/config/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.ehb.rental.rentalplatform.config;
+
+
+import com.ehb.rental.rentalplatform.model.User;
+import com.ehb.rental.rentalplatform.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole()) // like: USER or ADMIN
+                .build();
+    }
+}
+
